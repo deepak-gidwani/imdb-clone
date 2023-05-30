4e400d00720b7961f29129f8be2df6f5ef117ac7
Deepak Gidwani <deepakgidwani12356@gmail.com>
2023-05-30 17:46:01 +0530

    added throttling.py

---
+++ watchlist_app/api/throttling.py
@@ -0,0 +1,9 @@
+from rest_framework.throttling import UserRateThrottle
+
+class ReviewCreateThrottle(UserRateThrottle):
+    scope = 'review-create'
+    
+class ReviewListThrottle(UserRateThrottle):
+    scope = 'review-list'
+    
+#agr throttling ki alag se file ni bnani to scope throttling use kro
